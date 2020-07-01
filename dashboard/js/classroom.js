bd3c639076ae9acc2dbda0417263f2b9f81fc778
Mik_a <ilemik@youcanstar.com>
2020-07-01 16:31:38 +0900

    permission bug fix

--- dashboard/js/classroom.js
+++ dashboard/js/classroom.js
@@ -1585,37 +1585,64 @@ function OnClickStudent(div){
         $("#perbtn > .circle").clearQueue();
         
         if(e.target.dataset.classPermission == "true"){
-            $("#perbtn").css({
-                'background-color' : "#18dbbe"
-            })
-            $("#perbtn > .circle").css({
-                left: "22px"
-            })
-            $("#perbtn").addClass("on");
-            $("#perbtn").removeClass("off");
-            
-        }
-        else{
-            $("#perbtn").css({
-                'background-color' : "gray"
-            })
-            $("#perbtn > .circle").css({
-                left: "2px"
-            })
-            $("#perbtn").addClass("off");
-            $("#perbtn").removeClass("on");
-        }
-    
-        $(menu).css({
-            left: e.clientX,
-            top : e.clientY
-        })
-    
-        if(!$("#student-menu").is(':visible')){
-            $( "#student-menu" ).show( "blind", {}, 150, function(){});
-        }
-    
-        menu.getElementsByClassName("stuname")[0].innerHTML = name;
+          $("#classP").css({
+              'background-color' : "#18dbbe"
+          })
+          $("#classP > .circle").css({
+              left: "22px"
+          })
+          $("#classP").addClass("on");
+          $("#classP").removeClass("off");
+      }
+      else{
+          $("#classP").css({
+              'background-color' : "gray"
+          })
+          $("#classP > .circle").css({
+              left: "2px"
+          })
+          $("#classP").addClass("off");
+          $("#classP").removeClass("on");
+      }
+  
+  
+      $("#micP").clearQueue();
+      $("#micP > .circle").clearQueue();
+  
+      if(e.target.dataset.micPermission == "true"){
+          $("#micP").css({
+              'background-color' : "#18dbbe"
+          })
+          $("#micP > .circle").css({
+              left: "22px"
+          })
+          $("#micP").addClass("on");
+          $("#micP").removeClass("off");
+      }
+      else{
+          $("#micP").css({
+              'background-color' : "gray"
+          })
+          $("#micP > .circle").css({
+              left: "2px"
+          })
+          $("#micP").addClass("off");
+          $("#micP").removeClass("on");
+      }
+  
+  
+  
+  
+      $(menu).css({
+          left: e.clientX,
+          top : e.clientY
+      })
+  
+      if(!$("#student-menu").is(':visible')){
+          $( "#student-menu" ).show( "blind", {}, 150, function(){});
+      }
+  
+      menu.getElementsByClassName("stuname")[0].innerHTML = name;
     })
 }
 
