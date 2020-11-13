8aa76ef5fbff7603d54fff2473e6314e247ecabd
Mik_a <ilemik@youcanstar.com>
2020-11-13 15:55:12 +0900

    navigation open closed

--- dashboard/Components/PageNavigation.js
+++ dashboard/Components/PageNavigation.js
@@ -31,7 +31,7 @@ class NaviController extends React.Component {
 
 class PageNavigation extends React.Component {
     state = {
-        closed: false,
+        closed: true,
         currentIdx: 0
     }
 
@@ -58,15 +58,18 @@ class PageNavigation extends React.Component {
     }
 
     collapse(e) {
-        this.setState({ closed: !this.state.closed });
-        if (this.state.closed) {
-            $(pageNavigator.obj).animate({ "height": "95%" });
-            e.target.style.transform = "rotate(-90deg)";
-        }
-        else {
-            $(pageNavigator.obj).animate({ "height": "93px" });
-            e.target.style.transform = "rotate(90deg)";
-        }
+        let target = e.target;
+        this.setState({ closed: !this.state.closed },() => {
+            if (!this.state.closed) {
+                $(pageNavigator.obj).animate({ "height": "95%" });
+                target.style.transform = "rotate(-90deg)";
+            }
+            else {
+                $(pageNavigator.obj).animate({ "height": "93px" });
+                target.style.transform = "rotate(90deg)";
+            }
+        });
+
     };
 
     inputHandler() {
